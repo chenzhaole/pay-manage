da76065b78a768764ea723187191e64130072cf3
chen_zhl <chenzhaole>
2018-05-21 01:21:39 +0800

    整理代码

--- pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwDFController.java
+++ pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwDFController.java
@@ -32,15 +32,17 @@ public class GwDFController {
     private Logger logger = LoggerFactory.getLogger(GwDFController.class);
 
     @Autowired
-    private ITradeDFReqHandler tradeDFReqHandler;
+    private ITradeDFCreateHandler tradeDFCreateHandler;
     @Autowired
-    private ITradeDFBalanceHandler tradeDFBalanceHandler;
+    private ITradeDFBalancePlatHandler tradeDFBalancePlatHandler;
     @Autowired
-    private ITradeDFQueryOrderHandler tradeDFQueryOrderHandler;
+    private ITradeDFBalanceChanHandler tradeDFBalanceChanHandler;
+    @Autowired
+    private ITradeDFQueryPlatHandler tradeDFQueryPlatHandler;
+    @Autowired
+    private ITradeDFQueryChanHandler tradeDFQueryChanHandler;
     @Autowired
     private ITradeDFBatchHandler tradeDFBatchHandler;
-    @Autowired
-    private ITradeDFQueryHandler tradeDFQueryHandler;
     @Autowired
     private TaskLogService taskLogService;
 
@@ -61,7 +63,7 @@ public class GwDFController {
             logger.info("代付API，【代付请求接口】收到客户端请求参数后做url解码后的值为：data="+data);
 
             //校验请求参数
-            CommonResponse checkResp = tradeDFReqHandler.checkParam(data,ip);
+            CommonResponse checkResp = tradeDFCreateHandler.checkParam(data,ip);
             logger.info("代付API，【代付请求接口】校验请求参数的结果为："+ JSONObject.toJSONString(checkResp));
 
             if( !ErrorCodeEnum.SUCCESS.getCode().equals(checkResp.getRespCode())){
@@ -71,7 +73,7 @@ public class GwDFController {
             }else{
                 TradeDFCreateOrderRequest tradeRequest = (TradeDFCreateOrderRequest) checkResp.getData();
                 logger.info("代付API，【代付请求接口】传入的TradeProxyPayCreateOrderRequest信息为：TradeProxyPayCreateOrderRequest="+JSONObject.toJSONString(tradeRequest));
-                resp = JSON.parseObject(tradeDFReqHandler.req(tradeRequest, ip),DFCreateOrderResponse.class);
+                resp = JSON.parseObject(tradeDFCreateHandler.create(tradeRequest, ip),DFCreateOrderResponse.class);
                 logger.info("代付API，【代付请求接口】返回的ProxyPayCreateOrderResponse信息为：ProxyPayCreateOrderResponse="+JSONObject.toJSONString(resp));
             }
 
@@ -104,7 +106,7 @@ public class GwDFController {
 
 
             //校验请求参数
-            CommonResponse checkResp = tradeDFBalanceHandler.checkParam(data,ip);
+            CommonResponse checkResp = tradeDFBalancePlatHandler.checkParam(data,ip);
             logger.info("代付API，【代付余额查询接口】校验请求参数的结果为："+ JSONObject.toJSONString(checkResp));
 
             if( !ErrorCodeEnum.SUCCESS.getCode().equals(checkResp.getRespCode())){
@@ -114,7 +116,7 @@ public class GwDFController {
             }else{
                 TradeDFQueryBalanceRequest tradeRequest = (TradeDFQueryBalanceRequest) checkResp.getData();
                 logger.info("代付API，【代付余额查询接口】传入的TradeDFQueryBalanceRequest信息为：TradeDFQueryBalanceRequest="+JSONObject.toJSONString(tradeRequest));
-                resp = JSON.parseObject(tradeDFBalanceHandler.balance(tradeRequest, ip),DFQueryBalanceResponse.class);
+                resp = JSON.parseObject(tradeDFBalancePlatHandler.balance(tradeRequest, ip),DFQueryBalanceResponse.class);
                 logger.info("代付API，【代付余额查询接口】返回的DFQueryBalanceResponse信息为：DFQueryBalanceResponse="+JSONObject.toJSONString(resp));
             }
 
@@ -129,11 +131,11 @@ public class GwDFController {
     }
 
     /**
-     * 代付查单接口
+     * 代付订单,平台查询接口
      */
-    @RequestMapping(value="query",produces = "application/json;charset=utf-8")
+    @RequestMapping(value="queryPlat",produces = "application/json;charset=utf-8")
     @ResponseBody
-    public String query(@RequestBody String data,HttpServletRequest request){
+    public String queryPlat(@RequestBody String data,HttpServletRequest request){
         logger.info("代付API，【代付查单接口】收到客户端请求参数：data="+data);
         DFQueryOrderResponse resp = new DFQueryOrderResponse();
         DFQueryOrderResponse.DFQueryOrderResponseHead head = new DFQueryOrderResponse.DFQueryOrderResponseHead();
@@ -144,7 +146,7 @@ public class GwDFController {
             data = URLDecoder.decode(data, "utf-8");
             logger.info("代付API，【代付查单接口】收到客户端请求参数后做url解码后的值为：data="+data);
             //校验请求参数
-            CommonResponse checkResp = tradeDFQueryOrderHandler.checkParam(data,ip);
+            CommonResponse checkResp = tradeDFQueryPlatHandler.checkParam(data,ip);
             logger.info("代付API，【代付查单接口】校验请求参数的结果为："+ JSONObject.toJSONString(checkResp));
 
             if( !ErrorCodeEnum.SUCCESS.getCode().equals(checkResp.getRespCode())){
@@ -154,7 +156,7 @@ public class GwDFController {
             }else{
                 TradeDFQueryOrderRequest tradeRequest = (TradeDFQueryOrderRequest) checkResp.getData();
                 logger.info("代付API，【代付查单接口】传入的TradeDFQueryOrderRequest信息为：TradeDFQueryOrderRequest="+JSONObject.toJSONString(tradeRequest));
-                resp = JSON.parseObject(tradeDFQueryOrderHandler.query(tradeRequest, ip),DFQueryOrderResponse.class);
+                resp = JSON.parseObject(tradeDFQueryPlatHandler.query(tradeRequest, ip),DFQueryOrderResponse.class);
                 logger.info("代付API，【代付查单接口】返回的DFQueryOrderResponse信息为：DFQueryOrderResponse="+JSONObject.toJSONString(resp));
             }
         }catch(Exception e){
@@ -170,9 +172,9 @@ public class GwDFController {
     /**
      * 定时任务发起代付
      */
-    @RequestMapping("execProxyPay")
+    @RequestMapping("taskProxyPay")
     @ResponseBody
-    public String execProxyPay(@RequestParam(required = false,value = "id") Integer logId){
+    public String taskProxyPay(@RequestParam(required = false,value = "id") Integer logId){
         logger.info("代付API，【定时任务发起代付】任务执行logId开始："+logId);
         CommonResult result = tradeDFBatchHandler.process(null);
         taskLogService.recordLog(logId,result);
@@ -183,11 +185,11 @@ public class GwDFController {
     /**
      * 定时任务代付查单
      */
-    @RequestMapping("execQuery")
+    @RequestMapping("taskProxyQuery")
     @ResponseBody
-    public String execQuery(@RequestParam(required = false,value = "id") Integer logId){
+    public String taskProxyQuery(@RequestParam(required = false,value = "id") Integer logId){
         logger.info("代付API，【定时任务代付查单】任务执行logId开始："+logId);
-        CommonResult result = tradeDFQueryHandler.process(null);
+        CommonResult result = tradeDFQueryChanHandler.process(null);
         taskLogService.recordLog(logId,result);
         logger.info("代付API，【定时任务代付查单】任务执行logId结束："+logId+" "+JSON.toJSONString(result));
         return "ok";
