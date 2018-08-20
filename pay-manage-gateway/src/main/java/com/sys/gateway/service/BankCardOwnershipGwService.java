00918a4830f172b7d9037320a1f21e0d05b839bb
chen_zhl <chenzhaole>
2018-08-20 10:24:31 +0800

    删除垃圾代码

--- pay-manage-gateway/src/main/java/com/sys/gateway/service/BankCardOwnershipGwService.java
+++ pay-manage-gateway/src/main/java/com/sys/gateway/service/BankCardOwnershipGwService.java
@@ -2,11 +2,7 @@ package com.sys.gateway.service;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.sys.boss.api.entry.CommonResponse;
-import com.sys.boss.api.entry.CommonResult;
-import com.sys.boss.api.entry.trade.request.selectpay.TradeSelectPayRequest;
 import com.sys.boss.api.entry.trade.response.ownership.OwnershipResponse;
-import com.sys.boss.api.entry.trade.response.selectpay.SelectOrderCreateResponse;
 
 /**
  * @Description:网关接口业务处理接口
