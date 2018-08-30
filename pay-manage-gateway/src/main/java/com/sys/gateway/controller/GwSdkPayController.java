594bc7631f7eb2aaa8a1ee55496d2e2c0e74eb49
chen_zhl <chenzhaole>
2018-08-30 10:38:21 +0800

    支付SDK修改bug

--- pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwSdkPayController.java
+++ pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwSdkPayController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.sys.boss.api.entry.CommonResponse;
 import com.sys.common.enums.ErrorCodeEnum;
+import com.sys.common.util.RSAUtils;
+import com.sys.gateway.common.ConfigUtil;
 import com.sys.gateway.common.IpUtil;
 import com.sys.gateway.service.GwSdkPayService;
 import org.slf4j.Logger;
@@ -24,11 +26,16 @@ import java.util.Map;
 /**
  * 支付SDK创建订单接口
  */
+
 @Controller
 @RequestMapping(value = "")
 public class GwSdkPayController {
 
     protected final Logger logger = LoggerFactory.getLogger(GwSdkPayController.class);
+
+    private static final String PLAT_SDK_PRIVATE_KEY = ConfigUtil.getValue("plat_sdk_private_key");
+    private static final String CLIENT_SDK_PUBLIC_KEY = ConfigUtil.getValue("client_sdk_public_key");
+
     private final String BIZ_NAME = "支付SDK-创建支付订单-";
     @Autowired
     GwSdkPayService gwSdkPayService;
@@ -40,13 +47,17 @@ public class GwSdkPayController {
     @ResponseBody
     public String commPay(@RequestBody String data, HttpServletRequest request, HttpServletResponse response, RedirectAttributes redirectAttributes) throws java.io.IOException {
         Map rtnMap = new HashMap();
+        String rtnStr = "";
         try {
             String ip = IpUtil.getRemoteHost(request);//请求ip
             logger.info(BIZ_NAME + "客户端请求ip：" + ip);
             data = URLDecoder.decode(data, "utf-8");
-            logger.info(BIZ_NAME + "客户端请求参数后做url解码后的值为：" + data);
+            logger.info(BIZ_NAME + "客户端请求参数做url解码后的值为：" + data);
+            String srcData = RSAUtils.decrypt(data, PLAT_SDK_PRIVATE_KEY);
+            logger.info(BIZ_NAME + "客户端请求参数RSA解密后的值为：" + srcData);
+
             //校验请求参数
-            CommonResponse checkResp = gwSdkPayService.checkParam(data);
+            CommonResponse checkResp = gwSdkPayService.checkParam(srcData);
             logger.info(BIZ_NAME + "校验请求参数的结果为：" + JSONObject.toJSONString(checkResp));
             if (!ErrorCodeEnum.SUCCESS.getCode().equals(checkResp.getRespCode())) {
                 rtnMap.put("code", checkResp.getRespCode());
@@ -57,14 +68,19 @@ public class GwSdkPayController {
                 rtnMap = gwSdkPayService.pay(reqData, ip);
                 logger.info(BIZ_NAME + "返回信息：" + JSONObject.toJSONString(rtnMap));
             }
+
+            String rtnSrcStr = JSON.toJSONString(rtnMap);
+            logger.info(BIZ_NAME + "返回下游商户原始字符串：" + rtnSrcStr);
+            rtnStr = RSAUtils.encrypt(rtnSrcStr, CLIENT_SDK_PUBLIC_KEY);
+            logger.info(BIZ_NAME + "返回下游商户RSA加密后字符串：" + rtnStr);
         } catch (Exception e) {
             e.printStackTrace();
             logger.error(BIZ_NAME + "异常" + e.getMessage());
             rtnMap.put("code", ErrorCodeEnum.FAILURE.getCode());
             rtnMap.put("msg", ErrorCodeEnum.FAILURE.getDesc());
         }
-        logger.info(BIZ_NAME + "返回下游商户：" + JSON.toJSONString(rtnMap));
-        return JSON.toJSONString(rtnMap);
+
+        return rtnStr;
     }
 
 }
--- pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwSdkQueryController.java
+++ pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwSdkQueryController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.sys.boss.api.entry.CommonResponse;
 import com.sys.common.enums.ErrorCodeEnum;
+import com.sys.common.util.RSAUtils;
+import com.sys.gateway.common.ConfigUtil;
 import com.sys.gateway.common.IpUtil;
 import com.sys.gateway.service.GwSdkQueryService;
 import org.slf4j.Logger;
@@ -29,6 +31,10 @@ import java.util.Map;
 public class GwSdkQueryController {
 
 	protected final Logger logger = LoggerFactory.getLogger(GwSdkQueryController.class);
+
+	private static final String PLAT_SDK_PRIVATE_KEY = ConfigUtil.getValue("plat_sdk_private_key");
+	private static final String CLIENT_SDK_PUBLIC_KEY = ConfigUtil.getValue("client_sdk_public_key");
+
 	private final String BIZ_NAME = "支付SDK-查询支付订单-";
 	@Autowired
 	GwSdkQueryService gwSdkQueryService;
@@ -38,13 +44,17 @@ public class GwSdkQueryController {
     @ResponseBody
     public String commPay(@RequestBody String data, HttpServletRequest request, HttpServletResponse response,RedirectAttributes redirectAttributes)throws java.io.IOException {
 		Map rtnMap = new HashMap();
+		String rtnStr = "";
 		try {
 			String ip = IpUtil.getRemoteHost(request);//请求ip
 			logger.info(BIZ_NAME + "客户端请求ip：" + ip);
 			data = URLDecoder.decode(data, "utf-8");
 			logger.info(BIZ_NAME + "客户端请求参数后做url解码后的值为：" + data);
+			String srcData = RSAUtils.decrypt(data, PLAT_SDK_PRIVATE_KEY);
+			logger.info(BIZ_NAME + "客户端请求参数RSA解密后的值为：" + srcData);
+
 			//校验请求参数
-			CommonResponse checkResp = gwSdkQueryService.checkParam(data);
+			CommonResponse checkResp = gwSdkQueryService.checkParam(srcData);
 			logger.info(BIZ_NAME + "校验请求参数的结果为：" + JSONObject.toJSONString(checkResp));
 			if (!ErrorCodeEnum.SUCCESS.getCode().equals(checkResp.getRespCode())) {
 				rtnMap.put("code", checkResp.getRespCode());
@@ -55,14 +65,19 @@ public class GwSdkQueryController {
 				rtnMap = gwSdkQueryService.query(reqData, ip);
 				logger.info(BIZ_NAME + "返回信息：" + JSONObject.toJSONString(rtnMap));
 			}
+
+			String rtnSrcStr = JSON.toJSONString(rtnMap);
+			logger.info(BIZ_NAME + "返回下游商户原始字符串：" + rtnSrcStr);
+			rtnStr = RSAUtils.encrypt(rtnSrcStr, CLIENT_SDK_PUBLIC_KEY);
+			logger.info(BIZ_NAME + "返回下游商户RSA加密后字符串：" + rtnStr);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.error(BIZ_NAME + "异常" + e.getMessage());
 			rtnMap.put("code", ErrorCodeEnum.FAILURE.getCode());
 			rtnMap.put("msg", ErrorCodeEnum.FAILURE.getDesc());
 		}
-		logger.info(BIZ_NAME + "返回下游商户：" + JSON.toJSONString(rtnMap));
-		return JSON.toJSONString(rtnMap);
+		return rtnStr;
     }
 
 }
