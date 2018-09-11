5e6c0e9af2816eb22591567a15328c2007067409
xiaoqi.wang <hacker.528>
2018-09-11 18:53:19 +0800

    内部对账

---
+++ pay-manage-gateway/src/main/java/com/sys/gateway/controller/GwOrderAcctCheckingController.java
@@ -0,0 +1,19 @@
+package com.sys.gateway.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/orderAcctChecking")
+@Controller
+public class GwOrderAcctCheckingController extends GwCashierBaseController {
+
+
+
+
+
+
+
+
+
+}
